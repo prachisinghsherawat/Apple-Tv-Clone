e9763fd8cf576ef4a42bbc20b1356f3c27312d8d
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-16 23:00:01 +0530

    Home Details

--- src/componants/HomeDetails/HomeDetails.jsx
+++ src/componants/HomeDetails/HomeDetails.jsx
@@ -1,29 +1,42 @@
 
-// import React from "react";
-
-// export const HomeDetails = () =>{
-
-//   return (
-//     <div  className="playback">
-//       <video controls
-//         width="100%"
-//         height="100%" 
-//         muted
-//         loop
-//         autoplay >
-
-//         <source
-//             src={}
-//             type="video/mp4"
-//         />
-//         Your browser does not support the video tag.
-
-//       </video>
-//     </div>
-//   );
-// }
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {comingSoon} from "../Home/Data/Data"
 
 
+export const HomeDetails = () =>{
+
+    const {id} = useParams()
+    const [storedData , setStoredData] = useState([])
+    useEffect(() => {ComingSoonData()} , [])
+
+    let ComingSoonData = () => {
+
+        let filterData = comingSoon.filter((el)=> el.id == id )
+        setStoredData(filterData)
+    }
+
+  return (
+    <div  className="playback">
+      <video controls
+        width="100%"
+        height="100%" 
+        muted
+        loop
+        autoplay >
+
+        {storedData.map((el)=>(
+            <source
+            src={el.video}
+            type="video/mp4"/>
+        ))}
+
+        Your browser does not support the video tag.
+
+      </video>
+    </div>
+  );
+}
 
 
 
@@ -31,19 +44,21 @@
 
 
 
-// // export const HomeDetails = () => {
 
-// //     return(
 
-// //         <>
-// //             <div class="playback">
-// //             <video width="400" controls>
-// //                   <source src={Avengers} type="video/mp4" />
+// export const HomeDetails = () => {
+
+//     return(
+
+//         <>
+//             <div class="playback">
+//             <video width="400" controls>
+//                   <source src={Avengers} type="video/mp4" />
  
-// //                    Your browser does not support HTML video.
-// //             </video>
-// //             </div>
+//                    Your browser does not support HTML video.
+//             </video>
+//             </div>
 
-// //         </>
-// //     )
-// // }
+//         </>
+//     )
+// }
